3418e3bcb261d1216adc5bfff92fbf06caa042aa
aolyang <aolyang@foxmail.com>
2023-12-07 19:44:58 +0800

    update lsp config

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -4,8 +4,23 @@ local M = {
         require("core.utils").lazy_load("nvim-lspconfig")
     end,
     config = function()
+        local LspM = require("plugins.lsp.module")
+
         require("plugins.lsp.setup")
-        require("plugins.lsp.lua_ls.setup")
+        require("plugins.lsp.setup-lua")
+
+        -- remmember mason ensure installed
+        local languages = {
+            -- frontend
+            "tsserver",
+            "tailwindcss",
+        }
+        for _, lsp in ipairs(languages) do
+            require("lspconfig")[lsp].setup({
+                on_attach = LspM.on_attach,
+                capabilities = LspM.capabilities,
+            })
+        end
     end,
 }
 
