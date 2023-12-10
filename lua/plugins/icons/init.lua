05e3ba3e4a2a4d484c872f9d3deeb79b09d9d016
aolyang <aolyang@foxmail.com>
2023-12-10 20:57:01 +0800

    update icons config

--- lua/plugins/icons/init.lua
+++ lua/plugins/icons/init.lua
@@ -1,7 +1,7 @@
 local M = {
     "nvim-tree/nvim-web-devicons",
     opts = function()
-        return { override = require("nvchad.icons.devicons") }
+        return { override = require("plugins.icons.file") }
     end,
     config = function(_, opts)
         dofile(vim.g.base46_cache .. "devicons")
