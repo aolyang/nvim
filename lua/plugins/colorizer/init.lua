50225fab23a5eda5ca012539533c110f38a1fb21
aolyang <aolyang@foxmail.com>
2023-12-13 00:54:23 +0800

    setup colorizer

---
+++ lua/plugins/colorizer/init.lua
@@ -0,0 +1,9 @@
+local M = {
+    "norcalli/nvim-colorizer.lua",
+    setup = function()
+        -- https://github.com/norcalli/nvim-colorizer.lua
+        require("colorizer").setup()
+    end,
+}
+
+return M
