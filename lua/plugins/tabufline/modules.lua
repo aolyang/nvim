f0bb2231b0a40a98271beb7d9d28b4aeb0dfd077
aolyang <aolyang@foxmail.com>
2023-12-14 00:44:09 +0800

    remove nvchad theme toggle

--- lua/plugins/tabufline/modules.lua
+++ lua/plugins/tabufline/modules.lua
@@ -21,7 +21,6 @@ vim.cmd "function! TbNewTab(a,b,c,d) \n tabnew \n endfunction"
 vim.cmd "function! TbGotoTab(tabnr,b,c,d) \n execute a:tabnr ..'tabnext' \n endfunction"
 vim.cmd "function! TbTabClose(a,b,c,d) \n lua require('plugins.tabufline').closeAllBufs('closeTab') \n endfunction"
 vim.cmd "function! TbCloseAllBufs(a,b,c,d) \n lua require('plugins.tabufline').closeAllBufs() \n endfunction"
-vim.cmd "function! TbToggle_theme(a,b,c,d) \n lua require('base46').toggle_theme() \n endfunction"
 vim.cmd "function! TbToggleTabs(a,b,c,d) \n let g:TbTabsToggled = !g:TbTabsToggled | redrawtabline \n endfunction"
 
 -------------------------------------------------------- functions ------------------------------------------------------------
@@ -193,9 +192,8 @@ M.tablist = function()
 end
 
 M.buttons = function()
-  local toggle_themeBtn = "%@TbToggle_theme@%#TbLineThemeToggleBtn#" .. vim.g.toggle_theme_icon .. "%X"
   local CloseAllBufsBtn = "%@TbCloseAllBufs@%#TbLineCloseAllBufsBtn#" .. " 󰅖 " .. "%X"
-  return toggle_themeBtn .. CloseAllBufsBtn
+  return CloseAllBufsBtn
 end
 
 M.run = function()
